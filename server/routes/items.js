6a48511434aaffd66ac7a7b4425ae56b591e07d9
Harsha D Hegde <harshahegde10s@gmail.com>
2025-05-26 22:25:53 +0530

    Delete server directory

--- server/routes/items.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Example route for adding an item
-router.post('/', (req, res) => {
-  // Handle adding item logic
-});
-
-module.exports = router;
